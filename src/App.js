aa9cb6cd22ec9bf56c864d69811fb699ff8512fb
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2022-03-20 15:43:36 +0300

    changing weather pic

--- src/App.js
+++ src/App.js
@@ -53,7 +53,15 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -74,7 +82,7 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">15 c</div>
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
               <div className="weather">Sunny</div>
             </div>
           </div>
