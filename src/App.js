38c787e6273c0338f1dcd30464664aeef97fe56b
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2022-03-20 14:09:13 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ const api = {
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
   return (
     <div className="app">
       <main>
@@ -14,6 +46,10 @@ function App() {
         <div className="location-box">
           <div className="location">New York,US</div>
           <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15 c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
